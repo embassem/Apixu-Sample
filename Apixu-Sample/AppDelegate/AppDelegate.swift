27b8f90449ebee7a59a251fdb8c10fda3cfc9550
Bassem Abbas <em.bassem@gmail.com>
2018-11-07 22:37:15 +0200

    update launch scene

--- Apixu-Sample/AppDelegate/AppDelegate.swift
+++ Apixu-Sample/AppDelegate/AppDelegate.swift
@@ -15,9 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = Container.homeViewController()
+        window?.makeKeyAndVisible()
         self.initApp()
-        sleep(2)
+        // sleep(2)
         return true
     }
 
